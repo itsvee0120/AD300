3f747965ea9f6f130e9707953934449ec8b211f9
Violet Nguyen <nviolet0120@gmail.com>
2024-10-13 07:40:08 -0700

    details

--- Zoo/ZooDemoTest.java
+++ Zoo/ZooDemoTest.java
@@ -1,3 +1,4 @@
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import java.io.ByteArrayOutputStream;
@@ -62,7 +63,7 @@ class ZooDemoTest {
         assertEquals(expectedOutput, outputStream.toString().trim());
     }
 
-    @BeforeEach
+    @AfterEach
     void tearDown() {
         System.setOut(originalOut);
     }
